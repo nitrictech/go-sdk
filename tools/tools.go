b4216f3521340e1c972224d7fc4c6e9c9d158845
Tim Holm <tim.holm@nitric.io>
2021-02-16 21:32:52 +1100

    chore: Remove erroeneous tool dependency.

--- tools/tools.go
+++ tools/tools.go
@@ -3,6 +3,7 @@ package tools
 import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/onsi/ginkgo/ginkgo"
-	_ "github.com/golang/mock"
+
+	//_ "github.com/golang/mock"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 )
