85a495e573a9531b819a86fd94c9e23ce31bf6cc
Tim Holm <tim.holm@nitric.io>
2021-09-30 14:04:40 +1000

    feat(context): Context API implementation for go.

---
+++ faas/context_test.go
@@ -0,0 +1,193 @@
+// Copyright 2021 Nitric Technologies Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package faas
+
+import (
+	v1 "github.com/nitrictech/apis/go/nitric/v1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Faas", func() {
+	Context("triggerContextFromGrpcTriggerRequest", func() {
+		When("Translating an invalid request", func() {
+			ctx, err := triggerContextFromGrpcTriggerRequest(&v1.TriggerRequest{})
+
+			It("should return an error", func() {
+				Expect(err).Should(HaveOccurred())
+			})
+
+			It("should return nil context", func() {
+				Expect(ctx).To(BeNil())
+			})
+		})
+
+		When("Translating a Http request", func() {
+			ctx, err := triggerContextFromGrpcTriggerRequest(&v1.TriggerRequest{
+				Data:     []byte("Hello"),
+				MimeType: "text/plain",
+				Context: &v1.TriggerRequest_Http{
+					Http: &v1.HttpTriggerContext{
+						Method: "GET",
+						Path:   "/test/path",
+						Headers: map[string]*v1.HeaderValue{
+							"Content-Type": {
+								Value: []string{"text/plain"},
+							},
+						},
+						QueryParams: map[string]string{
+							"q": "my-query",
+						},
+					},
+				},
+			})
+
+			It("should not return an error", func() {
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("should have http context", func() {
+				Expect(ctx.Http()).ToNot(BeNil())
+			})
+
+			It("should have the provided data", func() {
+				Expect(ctx.Http().Request.Data()).To(BeEquivalentTo([]byte("Hello")))
+			})
+
+			It("should have the provided Method", func() {
+				Expect(ctx.Http().Request.Method()).To(Equal("GET"))
+			})
+
+			It("should have the provided path", func() {
+				Expect(ctx.Http().Request.Path()).To(Equal("/test/path"))
+			})
+
+			It("should have the provided headers", func() {
+				Expect(ctx.Http().Request.Headers()).To(BeEquivalentTo(map[string][]string{
+					"Content-Type": {"text/plain"},
+				}))
+			})
+
+			It("should have the provided query params", func() {
+				Expect(ctx.Http().Request.Query()).To(BeEquivalentTo(map[string][]string{
+					"q": {"my-query"},
+				}))
+			})
+		})
+
+		When("Translating a Topic request", func() {
+			ctx, err := triggerContextFromGrpcTriggerRequest(&v1.TriggerRequest{
+				Data:     []byte("Hello"),
+				MimeType: "text/plain",
+				Context: &v1.TriggerRequest_Topic{
+					Topic: &v1.TopicTriggerContext{
+						Topic: "test-topic",
+					},
+				},
+			})
+
+			It("should not return an error", func() {
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("should have Event context", func() {
+				Expect(ctx.Event()).ToNot(BeNil())
+			})
+
+			It("should have the provided data", func() {
+				Expect(ctx.Event().Request.Data()).To(BeEquivalentTo([]byte("Hello")))
+			})
+
+			It("should have the provided topic name", func() {
+				Expect(ctx.Event().Request.Topic()).To(Equal("test-topic"))
+			})
+		})
+	})
+
+	Context("triggerContextToGrpcTriggerResponse", func() {
+		When("Translating HttpContext", func() {
+			resp, err := triggerContextToGrpcTriggerResponse(&triggerContextImpl{
+				http: &HttpContext{
+					Response: &HttpResponse{
+						Status: 404,
+						Headers: map[string][]string{
+							"Content-Type": {"text/plain"},
+						},
+						Body: []byte("Not found"),
+					},
+				},
+			})
+
+			It("should not return an error", func() {
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("should have http context", func() {
+				Expect(resp.GetHttp()).ToNot(BeNil())
+			})
+
+			It("should have the provided status", func() {
+				Expect(resp.GetHttp().Status).To(Equal(int32(404)))
+			})
+
+			It("should have the provided headers", func() {
+				Expect(resp.GetHttp().GetHeaders()).To(BeEquivalentTo(map[string]*v1.HeaderValue{
+					"Content-Type": {Value: []string{"text/plain"}},
+				}))
+			})
+
+			// TODO: Deprecated, remove for v1
+			It("should have the provided old headers", func() {
+				Expect(resp.GetHttp().GetHeadersOld()).To(BeEquivalentTo(map[string]string{
+					"Content-Type": "text/plain",
+				}))
+			})
+		})
+
+		When("Translating EventContext", func() {
+			resp, err := triggerContextToGrpcTriggerResponse(&triggerContextImpl{
+				event: &EventContext{
+					Response: &EventResponse{
+						Success: false,
+					},
+				},
+			})
+
+			It("should not return an error", func() {
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("should have topic context", func() {
+				Expect(resp.GetTopic()).ToNot(BeNil())
+			})
+
+			It("should have the provided success status", func() {
+				Expect(resp.GetTopic().GetSuccess()).To(BeFalse())
+			})
+		})
+
+		When("Translating invalid context", func() {
+			resp, err := triggerContextToGrpcTriggerResponse(&triggerContextImpl{})
+
+			It("should return an error", func() {
+				Expect(err).Should(HaveOccurred())
+			})
+
+			It("should return a nil response", func() {
+				Expect(resp).To(BeNil())
+			})
+		})
+	})
+})
---
+++ faas/loop.go
@@ -0,0 +1,123 @@
+// Copyright 2021 Nitric Technologies Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package faas
+
+import (
+	"fmt"
+
+	pb "github.com/nitrictech/apis/go/nitric/v1"
+	"github.com/nitrictech/go-sdk/api/errors"
+	"github.com/nitrictech/go-sdk/api/errors/codes"
+)
+
+func faasLoop(stream pb.FaasService_TriggerStreamClient, f HandlerProvider, errorCh chan error) {
+
+	for {
+		// Block receiving a message
+		srvrMsg, err := stream.Recv()
+
+		if err != nil {
+			// TODO: Make sure we use the correct kind of error types here
+			errorCh <- errors.FromGrpcError(err)
+			break
+		}
+
+		clientMsg := &pb.ClientMessage{
+			Id: srvrMsg.GetId(),
+		}
+
+		// We have a trigger
+		if srvrMsg.GetTriggerRequest() != nil {
+			ctx, err := triggerContextFromGrpcTriggerRequest(srvrMsg.GetTriggerRequest())
+
+			if err != nil {
+				fmt.Println("There was an error reading the TriggerRequest", err)
+				// Return a bad request here...
+				errorCh <- errors.NewWithCause(
+					codes.Internal,
+					"faasLoop: error reading the TriggerRequest",
+					err,
+				)
+				break
+			}
+
+			// Interrogate the HandlerProvider to see if it is capable of handling this trigger
+			var funcErr error = nil
+			if ctx.Http() != nil && f.GetHttp() != nil {
+				// handle http
+				ctx.http, funcErr = f.GetHttp()(ctx.Http(), httpDummy)
+
+				if ctx.http == nil && funcErr == nil {
+					funcErr = fmt.Errorf("nil context returned from http handler")
+				}
+			} else if ctx.Event() != nil && f.GetEvent() != nil {
+				// handle event
+				ctx.event, funcErr = f.GetEvent()(ctx.Event(), eventDummy)
+
+				if ctx.event == nil && funcErr == nil {
+					funcErr = fmt.Errorf("nil context returned from event handler")
+				}
+			} else if f.GetDefault() != nil {
+				// handle trigger
+				var newCtx TriggerContext = nil
+				newCtx, funcErr = f.GetDefault()(ctx, triggerDummy)
+				if newCtx != nil {
+					// Update response context
+					ctx.event = newCtx.Event()
+					ctx.http = newCtx.Http()
+				} else if funcErr != nil {
+					funcErr = fmt.Errorf("nil context returned from trigger handler")
+				}
+			} else {
+				// No available handler for the event...
+				// This is not a panic error case, but we need to return
+				// a default unavailable response
+				funcErr = fmt.Errorf("no handler available for trigger type")
+			}
+
+			if funcErr != nil {
+				if ctx.http != nil {
+					ctx.http.Response.Body = []byte(funcErr.Error())
+					ctx.http.Response.Headers = map[string][]string{
+						"Content-Type": {"text/plain"},
+					}
+					ctx.http.Response.Status = 500
+				} else if ctx.event != nil {
+					ctx.event.Response.Success = false
+				}
+			}
+
+			triggerResponse, err := triggerContextToGrpcTriggerResponse(ctx)
+
+			if err != nil {
+				fmt.Println("Error translating handler response", err)
+				errorCh <- errors.FromGrpcError(err)
+				break
+			}
+
+			clientMsg.Content = &pb.ClientMessage_TriggerResponse{
+				TriggerResponse: triggerResponse,
+			}
+
+			if err := stream.Send(clientMsg); err != nil {
+				fmt.Println("Failed to send msg", err)
+				errorCh <- errors.FromGrpcError(err)
+				break
+			}
+		} else if srvrMsg.GetInitResponse() != nil {
+			fmt.Println("Function connected to membrane", err)
+		}
+	}
+}
