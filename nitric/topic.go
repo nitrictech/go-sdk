6c544302f7c2665e6f61629ede699c17bb731076
David Moore <davemooreuws@gmail.com>
2024-07-08 11:28:12 +1000

    fix: ensure topics and subscriptions can be created in separate services

--- nitric/topic.go
+++ nitric/topic.go
@@ -15,7 +15,6 @@
 package nitric
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/nitrictech/go-sdk/api/topics"
@@ -52,48 +51,37 @@ type topic struct {
 }
 
 type subscribableTopic struct {
-	name    string
-	manager Manager
+	name         string
+	manager      Manager
+	registerChan chan RegisterResult
 }
 
 // NewTopic creates a new Topic with the give permissions.
 func NewTopic(name string) SubscribableTopic {
-	return &subscribableTopic{
-		name:    name,
-		manager: defaultManager,
+	topic := &subscribableTopic{
+		name:         name,
+		manager:      defaultManager,
+		registerChan: make(chan RegisterResult),
 	}
+
+	go defaultManager.registerResource(&v1.ResourceDeclareRequest{
+		Id: &v1.ResourceIdentifier{
+			Type: v1.ResourceType_Topic,
+			Name: name,
+		},
+		Config: &v1.ResourceDeclareRequest_Topic{
+			Topic: &v1.TopicResource{},
+		},
+	}, topic.registerChan)
+
+	return topic
 }
 
 func (t *subscribableTopic) Allow(permission TopicPermission, permissions ...TopicPermission) (Topic, error) {
 	allPerms := append([]TopicPermission{permission}, permissions...)
 
-	return defaultManager.newTopic(t.name, allPerms...)
-}
-
-func (m *manager) newTopic(name string, permissions ...TopicPermission) (Topic, error) {
-	rsc, err := m.resourceServiceClient()
-	if err != nil {
-		return nil, err
-	}
-
-	res := &v1.ResourceIdentifier{
-		Type: v1.ResourceType_Topic,
-		Name: name,
-	}
-
-	dr := &v1.ResourceDeclareRequest{
-		Id: res,
-		Config: &v1.ResourceDeclareRequest_Topic{
-			Topic: &v1.TopicResource{},
-		},
-	}
-	_, err = rsc.Declare(context.Background(), dr)
-	if err != nil {
-		return nil, err
-	}
-
 	actions := []v1.Action{}
-	for _, perm := range permissions {
+	for _, perm := range allPerms {
 		switch perm {
 		case TopicPublish:
 			actions = append(actions, v1.Action_TopicPublish)
@@ -102,7 +90,12 @@ func (m *manager) newTopic(name string, permissions ...TopicPermission) (Topic, 
 		}
 	}
 
-	_, err = rsc.Declare(context.Background(), functionResourceDeclareRequest(res, actions))
+	registerResult := <-t.registerChan
+	if registerResult.Err != nil {
+		return nil, registerResult.Err
+	}
+
+	m, err := t.manager.registerPolicy(registerResult.Identifier, actions...)
 	if err != nil {
 		return nil, err
 	}
@@ -116,7 +109,7 @@ func (m *manager) newTopic(name string, permissions ...TopicPermission) (Topic, 
 	}
 
 	return &topic{
-		Topic:   m.topics.Topic(name),
+		Topic:   m.topics.Topic(t.name),
 		manager: m,
 	}, nil
 }
